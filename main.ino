41c8ef3769583aeaa603e1f378faf8060f61732e
Simon Jamain <jamain.simon@gmail.com>
2019-01-28 19:41:28 +0100

    lol

--- main.ino
+++ main.ino
@@ -23,6 +23,21 @@ void setup() {
 
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
   //Mosfetv2wifiserver::begin();
+  display.clearDisplay();
+
+  display.setTextSize(1);      // Normal 1:1 pixel scale
+  display.setTextColor(WHITE); // Draw white text
+  display.setCursor(0, 0);     // Start at top-left corner
+  display.cp437(true);         // Use full 256 char 'Code Page 437' font
+
+  // Not all the characters will fit on the display. This is normal.
+  // Library will draw what it can and the rest will be clipped.
+  for(int16_t i=0; i<256; i++) {
+    if(i == '\n') display.write(' ');
+    else          display.write(i);
+  }
+
+  display.display();
 }
 
 // the loop function runs over and over again forever
